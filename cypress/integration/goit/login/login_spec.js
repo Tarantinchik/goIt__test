fc547b163d4d936bfe99883d74923ef5d8d147cd
Tarantinchik <denis.bykanov@gmail.com>
2021-10-05 12:55:49 -0500

    finished restore_spec.js

--- cypress/integration/goit/login/login_spec.js
+++ cypress/integration/goit/login/login_spec.js
@@ -25,12 +25,32 @@ context('', () => {
 
     it('', () => {
         cy.findByText('Вход').should('be.visible');
-        cy.get('label[for="user_email"]').should('have.text','Email');
-        cy.get('label[for="user_password"]').should('have.text','Пароль');
+        cy.get('label[for="user_email"]').should('have.text', 'Email');
+        cy.get('label[for="user_password"]').should('have.text', 'Пароль');
         cy.get('#user_email').should('be.visible');
         cy.get('#user_password').should('be.visible');
         cy.get('button[type="button"]').should('be.visible');
-        cy.get('button[type="submit"]').should('have.text','Войти');
-        cy.get('a[href="/account/password/restore"]').should('have.text','Не помню пароль');
+        cy.get('button[type="submit"]').should('have.text', 'Войти');
+        cy.get('a[href="/account/password/restore"]').should('have.text', 'Не помню пароль');
+
     })
+
+
+});
+context('', () => {
+    beforeEach(() => {
+        cy.openLoginAccountPage(1280, 720);
+        cy.get('button[type="submit"]').should('have.text', 'Войти').click();
+    });
+
+    it('', () => {
+         cy.get('div[label="Email"]').parent().find('span').eq(1).should('have.text', 'E-mail отсутствует');
+         cy.get('div[label="Пароль"]').parent().find('span').eq(1).should('have.text', 'Введи пароль, чтобы продолжить');
+
+    });
+    // cy.get('label[for="user_password"]').should('have.text','Введи пароль, чтобы продолжить');
+
+
+
+
 });
--- cypress/integration/goit/restore/restore_spec.js
+++ cypress/integration/goit/restore/restore_spec.js
@@ -20,6 +20,18 @@ import '../../../support/commands'
 context('', () => {
     beforeEach(() => {
         cy.openPasswordRestore(1280, 720);
+        cy.get('#user_email').type('2123');
+        cy.get('button[type="submit"]').click();
+    });
+
+    it('', () => {
+        cy.get('div[role="status"]').should('be.visible');
+    })
+});
+
+context('', () => {
+    beforeEach(() => {
+        cy.openPasswordRestore(1280, 720);
     });
 
     it('', () => {
@@ -38,12 +50,25 @@ context('', () => {
         cy.openPasswordRestore(1280, 720);
         cy.get('#user_email').type('pinchuk.dap@gmail.com');
         cy.get('button[type="submit"]').should('have.text','Отправить ссылку').click();
+        cy.wait(4000);
     });
 
     it('', () => {
-        cy.get('#__next').contains('Проверьте почту');
-        cy.get('#__next').contains('Мы отправили инструкцию и ссылку для смены пароля на почту');
+        cy.get('div[id="__next"]').parent().find('h1').should('have.text', 'Проверьте почту');
+        cy.get('div[id="__next"]').parent().find('p').should('contain', 'Мы отправили инструкцию и ссылку для смены пароля на почту');
+        cy.get('div[id="__next"]').parent().find('p').should('contain', 'pinchuk.dap@gmail.com');
 
+    })
+});
+
+context('', () => {
+    beforeEach(() => {
+        cy.openPasswordRestore(1280, 720);
+        cy.get('#user_email').type('amakatyor@gmail.com');
+        cy.get('button[type="submit"]').click();
+    });
+    it('', () => {
+        cy.get('div[role="status"]').should('be.visible');
     })
 });
 
